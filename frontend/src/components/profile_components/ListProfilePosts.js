8d27ba38b559973833a64ccd253e44920ca7a616
sewagodimo <edneciamat@gmail.com>
2019-06-22 16:51:31 +0200

    add the modal components to the profiles view

--- frontend/src/components/profile_components/ListProfilePosts.js
+++ frontend/src/components/profile_components/ListProfilePosts.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Card, Media, CardDeck } from 'reactstrap';
-import Avatar from 'react-avatar';
+import {Media} from 'reactstrap';
 import '../../styles/profile.css';
+import PostModal from './PostModal';
 
 class ListProfilePosts extends React.Component{
 
 	constructor(props) {
         super(props);
         this.state = {is_userview: false,
+                      modalShow: [],
                     };
     
                 }
@@ -15,15 +16,34 @@ class ListProfilePosts extends React.Component{
     getUrl = (type,image) => {
         return image.substring(image.indexOf(type))
     }
-    
+    renderModal = (post) => {
+        let modal;
+        if(this.state.modalShow[0]== true && this.state.modalShow[1]==post.id){
+            modal = <PostModal
+                show={this.state.modalShow}
+                image={this.getUrl("/posts/",post.image)}
+                id={post.id}
+                user={post.user}
+                caption={post.caption}
+              />
+        }
+        else{
+            modal = <p></p>
+        }
+       return modal 
+    }
 	
 	render(){ 
-		
+        let modalClose = () => this.setState({ modalShow: false });
 	return this.props.posts.map((post) => ( 
-		
+
 		<div className="cell" key={post.id}>
-			<Media src={this.getUrl("/posts/",post.image)} style={{width:'100%', height:'150px'}}/>
-		</div>
+            <Media src={this.getUrl("/posts/",post.image)} 
+            onClick={() => this.setState({ modalShow: [true, post.id]})}
+            style={{width:'100%', height:'150px'}}/>
+
+            {this.renderModal(post)}
+        </div>
 			
 		))
 	
